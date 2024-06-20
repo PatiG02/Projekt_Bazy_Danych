91eda77746dc941b10eac63248e512f4c9a4c009
andygoralczyk <cto@asinit.com>
2024-06-21 01:02:51 +0200

    test push

--- Projekt_Biuro_Turystyczne/src/dao/DBConnector.java
+++ Projekt_Biuro_Turystyczne/src/dao/DBConnector.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class DBConnector {
     private static final String URL = "jdbc:mysql://localhost:3306/biuro_turystyczne";
     private static final String USER = "root";
-    private static final String PASSWORD = "Patrycja#02";
+    private static final String PASSWORD = "";//"Patrycja#02";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
