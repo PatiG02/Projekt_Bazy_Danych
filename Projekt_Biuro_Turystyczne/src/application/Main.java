72c0d765f94c9dc5d4088a44baf6c955f38eecaf
andygoralczyk <cto@asinit.com>
2024-06-21 03:07:59 +0200

    update to the app

--- Projekt_Biuro_Turystyczne/src/application/Main.java
+++ Projekt_Biuro_Turystyczne/src/application/Main.java
@@ -2,22 +2,26 @@ package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) {
-        try {
-            primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Adres.fxml"))));
-            primaryStage.setTitle("Adres Management");
-            primaryStage.show();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+        primaryStage.setTitle("Biuro Turystyczne");
+
+        // Set the icon
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/pelican_logo.png")));
+
+        // Set the scene and window size
+        Scene scene = new Scene(root, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
